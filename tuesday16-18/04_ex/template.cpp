d7d46b672146a1129d3ade4c40c5e9c276df4e3b
Kristóf Umann <dkszelethus@gmail.com>
2019-05-02 15:37:38 +0200

    clang-format

--- tuesday16-18/04_ex/template.cpp
+++ tuesday16-18/04_ex/template.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 
-template <class T>
-void swap(T &a, T &b) {
-	T tmp = a;
-	a = b;
-	b = tmp;
+template <class T> void swap(T &a, T &b) {
+  T tmp = a;
+  a = b;
+  b = tmp;
 }
 
-template <int N>
-void f() {
-	std::cout << N << '\n';
-}
+template <int N> void f() { std::cout << N << '\n'; }
 
 int main() {
-	int a = 5, b = 6;
-	swap(a, b);
+  int a = 5, b = 6;
+  swap(a, b);
 
-	double d1 = 5.6, d2 = 9.3;
-	
-	swap(d1, d2);
-	
-	f<20>();
-}
+  double d1 = 5.6, d2 = 9.3;
+
+  swap(d1, d2);
+
+  f<20>();
+}
+
--- tuesday16-18/04_ex/vector.cpp
+++ tuesday16-18/04_ex/vector.cpp
@@ -1,17 +1,17 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 int main() {
-	std::vector<int> v = {0, 1, 2, 3, 3, 3, 3, 2, 7, 4, 5};
-	
-	for (std::vector<int>::iterator it = v.begin(); it != v.end();) {
-		if (*it == 3)
-			it = v.erase(it);
-		else
-			++it;
-	}
-	
-	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-		std::cout << *it << '\n';
-	std::cout << '\n';
+  std::vector<int> v = {0, 1, 2, 3, 3, 3, 3, 2, 7, 4, 5};
+
+  for (std::vector<int>::iterator it = v.begin(); it != v.end();) {
+    if (*it == 3)
+      it = v.erase(it);
+    else
+      ++it;
+  }
+
+  for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+    std::cout << *it << '\n';
+  std::cout << '\n';
 }
