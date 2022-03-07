559441fa3d95a1c87023c5e486ac7ef867e2b4c7
Kristóf Umann <dkszelethus@gmail.com>
2022-03-07 14:49:44 +0100

    fix an oopsie, post the solution

--- monday12-14/04gy/main.cpp
+++ monday12-14/04gy/main.cpp
@@ -24,12 +24,33 @@ int main() {
 } 
 
 char encrypt(char c) {
-  std::string from = {'a', 'e', 'i', 'o', 'u'};
-  std::string to   = {'u', 'a', 'e', 'i', 'o'};
+  std::string from;
+  std::string to;
+
+  std::ifstream fromfs("from.txt");
+  std::ifstream tofs("to.txt");
+
+  if (fromfs.fail()) {
+    std::cout << "Failed to open 'from.txt'!\n";
+    return '-';
+  }
+
+  if (tofs.fail()) {
+    std::cout << "Failed to open 'to.txt'!\n";
+    return '-';
+  }
+
+  std::getline(fromfs, from);
+  std::getline(tofs, to);
+
+  if (from.size() != to.size()) {
+    std::cout << "From and To must have the same length!\n";
+    return '-';
+  }
 
   char output = c;
 
-  for (int i = 0; i < sizeof(from) / sizeof(from[0]); ++i) {
+  for (int i = 0; i < from.size(); ++i) {
     if (c == from[i]) {
       output = to[i];
       break;
