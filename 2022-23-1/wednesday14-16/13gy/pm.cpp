2ba23153f306ff3e6ab29f480fcd1af3edb5d9bb
Kristóf Umann <dkszelethus@gmail.com>
2022-12-07 14:07:18 +0100

    pm.cpp

---
+++ 2022-23-1/wednesday14-16/13gy/pm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+std::vector<int> generateVector();
+
+int main() {
+  std::vector<int> vec = generateVector();
+
+  // (1a) STL algoritmus segítségével keressük meg az első elemet vec-ben,
+  // amelynek az értéke 3nál nagyobb!
+  
+  // (1b) Töröljük ki a vectorból ezt, és a rákövetkező elemet is!
+
+  // (3) Írjunk egy polimorfikus osztályt!
+  
+  // (4) Írjunk egy int-eket tartalmazó halmazt STL konténer segítségével,
+  // mely a 7 szerinti maradék szerint rendez!
+
+  // (5) Mi a különbség az std::remove_if és az std::stable_partition között?
+}
