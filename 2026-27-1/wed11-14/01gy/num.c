949c539f1a296233fd5d38fcf0b72c24150e4d00
Kristóf Umann <dkszelethus@gmail.com>
2026-09-10 10:48:39 +0200

    1st lecture

---
+++ 2026-27-1/wed11-14/01gy/num.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int i;
+  scanf("%d", &i);
+
+	int j;
+  scanf("%d", &j);
+	int k;
+  scanf("%d", &k);
+  int sz = i*j*k;
+  printf("%d");
+}
---
+++ 2026-27-1/wed8-11/01gy/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  float f = 5.0 / 2.0;
+  printf("%f\n", f);
+}
